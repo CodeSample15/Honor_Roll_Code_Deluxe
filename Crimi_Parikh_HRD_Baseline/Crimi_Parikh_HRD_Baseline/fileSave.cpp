2d3c38f6dae0a11adbc32782cd69806494eec6aa
CodeSample15 <lukecrimi15@gmail.com>
2021-06-08 22:47:15 -0400

    First version of GetStudentData finished

--- Crimi_Parikh_HRD_Baseline/Crimi_Parikh_HRD_Baseline/fileSave.cpp
+++ Crimi_Parikh_HRD_Baseline/Crimi_Parikh_HRD_Baseline/fileSave.cpp
@@ -23,6 +23,7 @@ void fileSave::saveStudentData(student& data)
 	/*
 	*	Order of data saved into file:
 	* 
+	*	'-' symbol indicating the start of a new student section
 	*	FirstName LastName
 	*	Number_Of_Classes_Taken (use this to get the number of lines remaining for that student)
 	*	Class1
@@ -32,7 +33,6 @@ void fileSave::saveStudentData(student& data)
 	*	... (rest of the classes)
 	*	Average
 	*	1 or 0 representing whether or not the student had a disciplinary issue
-	*	Student's location in the file for easy retrieval of data
 	*/
 
 	//finding out what number student this is
@@ -52,6 +52,7 @@ void fileSave::saveStudentData(student& data)
 	fout.open(fileSave::filePath, ios::app);
 
 	//save data to file
+	fout << '-' << endl;
 	fout << name << endl;
 	fout << classes.size() << endl;
 
@@ -63,7 +64,85 @@ void fileSave::saveStudentData(student& data)
 
 	fout << average << endl;
 	fout << (disciplineIssue ? "1" : "0") << endl;
-	fout << 
 
 	//close the file stream
+	fout.close();
+}
+
+string fileSave::getStudentData(int studentNum, vector<string>& classes, vector<double>& grades, double& average, bool& disciplineIssue)
+{
+	//opening the file in read mode
+	ifstream fin;
+	fin.open(fileSave::filePath, ios::in);
+
+	/*Logic for the get student data algorithm*/
+	//loop through the file and search for the data of the correct student
+		//finds the right number student (if there is one) and reads the data from the file
+	
+	//for keeping track what student the program is currently reading
+	int currentStudent = 1;
+	bool foundStudent = false;
+	string studentName = ""; //for recording the name of the found student
+
+	while (!fin.eof()) {
+		getline(fin, studentName); //scan one line down in the file
+
+		if (currentStudent == studentNum) {
+			getline(fin, studentName); //recording the student's name
+			foundStudent = true;
+			break;
+		}
+		else {
+			string currentLine = "";
+			currentStudent++;
+			do {
+				getline(fin, currentLine);
+			} while (currentLine != "-" && !fin.eof());
+		}
+	}
+
+
+	//returns empty data if the studentNum is invalid
+	//else the method returns the name of the student and fills the two vectors with the appropriate data
+	if (foundStudent) {
+		//record the data
+		string classNumber;
+		getline(fin, classNumber);
+		int numberOfClasses = stoi(classNumber);
+
+		//getting the classnames
+		classes.clear();
+		grades.clear();
+		string lineData = "";
+
+		for (int i = 0; i < numberOfClasses; i++) {
+			getline(fin, lineData);
+			classes.push_back(lineData);
+		}
+
+		//getting the grades
+		int grade = 0;
+		for (int i = 0; i < numberOfClasses; i++) {
+			getline(fin, lineData);
+			grade = stoi(lineData); //converting the string data to an integer
+			grades.push_back(grade);
+		}
+
+		//getting the average and if there is a discipline issue with the student
+
+		//average
+		string averageString = "";
+		getline(fin, averageString);
+		average = stod(averageString);
+
+		//disipline issue
+		string issue = "";
+		getline(fin, issue);
+		disciplineIssue = issue == "1";
+		
+		//return the name of the student found
+		return studentName;
+	}
+	
+	return "";
 }
--- Crimi_Parikh_HRD_Baseline/Crimi_Parikh_HRD_Baseline/fileSave.h
+++ Crimi_Parikh_HRD_Baseline/Crimi_Parikh_HRD_Baseline/fileSave.h
@@ -16,7 +16,7 @@ public:
 
 	//methods
 	void saveStudentData(student &data);
-	void getStudentData(int studentNum, vector<string>& classes, vector<double>& grades);
+	string getStudentData(int studentNum, vector<string>& classes, vector<double>& grades, double& average, bool& disciplineIssue); //returns the full name of the student, as well as filling the classes and grades vectors with the appropriate student data
 
 private:
 	string filePath;
